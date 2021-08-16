aa621e4726f248230a1339d5d18b464e83fd9700
mrharmtz <mrharmtz@gmail.com>
2021-08-16 23:03:03 +0300

    *added debug printing *changed algorithm to support iterators *added casting from container to iterator *completed constructors *added include guard *added test code

--- main.cpp
+++ main.cpp
@@ -12,6 +12,15 @@ int main(int argc, const char* argv[]){
     for(unsigned i = 0 ; i < (1 << order.size()) ; ++i)
         cout << "i(" << i << ") = " << (value = unsigned_shuffled_counter::next_value(value, order)) << endl;
 
+    try{
+        cout << "constructing my_counter" << endl;
+        unsigned_shuffled_counter my_counter(order.begin(), order.end());
+        cout << "constructing my_other_counter" << endl;
+        unsigned_shuffled_counter my_other_counter(order);
+    }catch(...){
+        cout << "oops an exception was thrown" << endl;
+    }
+
 
     return 0;
 }
--- shuffled_counter.hpp
+++ shuffled_counter.hpp
@@ -1,6 +1,24 @@
+#ifndef _SHUFFLE_COUNTER_HPP_
+#define _SHUFFLE_COUNTER_HPP_
 #include <array>
 #include <memory>
 #include <algorithm>
+
+#define _SHUFFLE_COUNTER_DEBUG_
+
+#ifdef _SHUFFLE_COUNTER_DEBUG_
+#include <cstdio>
+
+#define CNTR_DBG_PRINTLINE(format) printf("%s[%10d]: " format "\n", __func__, __LINE__)
+#define CNTR_DBG_FORMATLINE(format, ...) printf("%s[%10d]: " format "\n", __func__, __LINE__, __VA_ARGS__)
+
+#else
+
+#define CNTR_DBG_PRINTLINE(format) 
+#define CNTR_DBG_FORMATLINE(format, ...) 
+
+#endif
+
 
 template <typename BaseType, typename IndexType=BaseType, typename Allocator=std::allocator<IndexType>>
 class shuffled_counter
@@ -12,14 +30,14 @@ private:
     Allocator _allocator;
 public:
 
-    template <typename Container>
-    static BaseType next_value(BaseType value, const Container& order, bool inc_ndec=true){
+    template <typename Iterator>
+    static BaseType next_value(BaseType value, Iterator order, Iterator end_order, bool inc_ndec=true){
         
         BaseType mask, bit;
 
-        for (const auto& index : order)
+        for (; order != end_order ; ++order)
         {
-            mask = 1 << index;
+            mask = 1 << *order;
             
             if(!(value & mask)){
                 value |= mask;
@@ -33,27 +51,88 @@ public:
         return value;
     }
 
+    template <typename Container>
+    static BaseType next_value(BaseType value, const Container& order, bool inc_ndec=true){
+        
+        return next_value(value, std::cbegin(order), std::cend(order), inc_ndec);
+    }
+
     template <typename Iterator>
     shuffled_counter(Iterator begin, Iterator end, BaseType value=0, const Allocator& allocatr = Allocator())
     :_value(value), _shuffle_order(NULL), _size(std::distance(begin, end)), _allocator(allocatr){
-        _shuffle_order = allocatr.allocate(_size);
+        using traits_t = std::allocator_traits<decltype(_allocator)>;
+        _shuffle_order = _allocator.allocate(_size);
+        traits_t::construct(_allocator, _shuffle_order, 0);
 
-        for (; begin != end; ++begin)
-        {
-            /* code */
-        }
+        CNTR_DBG_PRINTLINE("checking order");
         
+        for (Iterator index = begin; index != end; ++index){
 
+            CNTR_DBG_FORMATLINE("index = %lu, _size = %lu", *index, _size);
+            
+            if (*index >= _size)
+                throw std::invalid_argument("index is greater than size");
+
+            if(_shuffle_order[*index])
+                throw std::invalid_argument("duplicate values in order");
+
+            _shuffle_order[*index] = 1;
+        }
+            
+        CNTR_DBG_PRINTLINE("order passed check, initializing _shuffle_order");
+
+        IndexType* order = _shuffle_order;
+        for (Iterator index = begin; index != end; ++index, ++order){
+            *order = *index;
+        }
+
+        #ifdef _SHUFFLE_COUNTER_DEBUG_
+        for (IndexType* order = _shuffle_order; order != _shuffle_order + _size; ++order)
+            CNTR_DBG_FORMATLINE("shuffle_order = %lu", *order);
+        #endif
     }
 
     template <typename Container>
     shuffled_counter(Container container, BaseType value=0, const Allocator& allocatr = Allocator())
     :_value(value), _shuffle_order(NULL), _size(std::size(container)), _allocator(allocatr){
+        using traits_t = std::allocator_traits<decltype(_allocator)>;
+        _shuffle_order = _allocator.allocate(_size);
+        traits_t::construct(_allocator, _shuffle_order, 0);
 
+        CNTR_DBG_PRINTLINE("checking order");
+        
+        for (auto index : container){
+
+            CNTR_DBG_FORMATLINE("index = %lu, _size = %lu", index, _size);
+            
+            if (index >= _size)
+                throw std::invalid_argument("index is greater than size");
+
+            if(_shuffle_order[index])
+                throw std::invalid_argument("duplicate values in order");
+
+            _shuffle_order[index] = 1;
+        }
+            
+        CNTR_DBG_PRINTLINE("order passed check, initializing _shuffle_order");
+
+        IndexType* order = _shuffle_order;
+        for (auto index : container){
+            *order++ = index;
+        }
+
+        #ifdef _SHUFFLE_COUNTER_DEBUG_
+        for (IndexType* order = _shuffle_order; order != _shuffle_order + _size; ++order)
+            CNTR_DBG_FORMATLINE("shuffle_order = %lu", *order);
+        #endif
     }
+
     ~shuffled_counter(){
         delete[] _shuffle_order;
     }
 };
 
 typedef shuffled_counter<unsigned> unsigned_shuffled_counter;
+
+
+#endif //_SHUFFLE_COUNTER_HPP_
